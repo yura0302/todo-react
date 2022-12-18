e8341729d91ae3384070c432abe0de7b32ad4ee2
yura <rladbfk0000@naver.com>
2022-12-18 23:23:33 +0900

    feat :input창 공백 입력 방지

--- src/components/AddTodo/AddTodo.tsx
+++ src/components/AddTodo/AddTodo.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Todos } from "../TodoList/TodoList";
-import { addSyntheticLeadingComment } from "typescript";
 
 interface Props {
   onAdd: (todo: Todos) => void;
@@ -14,7 +13,11 @@ export default function AddTodo({ onAdd }: Props) {
     setText(e.target.value);
   const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (text.trim().length === 0) {
+      return;
+    }
     onAdd({ id: "고유한값", text, status: "active" });
+    setText("");
   };
 
   return (
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -7,14 +7,13 @@ export interface Todos {
   status: string;
 }
 
-/** 이 객체 값이 없을 때를 생각해서 undefined 지정 */
+/** 이객체 값이 없을 때를 생각해서 undefined 지정 */
 export default function ToDoList() {
   const [todos, setTodos] = useState<Todos[] | any>([
     { id: "123", text: "장보기", status: "active" },
     { id: "124", text: "공부하기", status: "active" },
   ]);
 
-  /** 이 객체 값이 없을 때를 생각해서 undefined 지정 */
   const handleAdd = (todo: Todos) =>
     // 새로운 투두를 todos에 업데이트 해야함
     // console.log(todo);
